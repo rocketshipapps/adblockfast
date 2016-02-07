8383081d046b67fb37e96f141f9931314698a892
Brian Kennish <oldestlivingboy@gmail.com>
2016-02-07 14:57:01 -0800

    Cleanup.

--- android/app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
+++ android/app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
@@ -4,7 +4,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
