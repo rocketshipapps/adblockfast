721d74d04101ee21213feff40289b2608a241b6f
Brian Kennish <oldestlivingboy@gmail.com>
2023-10-29 17:56:07 -0400

    Revert "Revert "Upgraded to AndroidX.""
    This reverts commit c40ce4d0a14daeefaec1cc33ea4f58f5e6a706f8.

--- android/app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
+++ android/app/src/main/java/com/rocketshipapps/adblockfast/service/NotificationService.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.rocketshipapps.adblockfast.MainActivity;
