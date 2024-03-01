375301ad692929a9d3c44610dd39d0bd8dcacd3f
Brian Kennish <oldestlivingboy@gmail.com>
2024-03-01 15:09:17 -0500

    Restored current update behavior.

--- android/app/src/main/java/com/rocketshipapps/adblockfast/service/UpdateReceiver.java
+++ android/app/src/main/java/com/rocketshipapps/adblockfast/service/UpdateReceiver.java
@@ -4,11 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.rocketshipapps.adblockfast.AdblockFastApplication;
+
 public class UpdateReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if ("android.intent.action.PACKAGE_REPLACED".equals(intent.getAction())) {
             // TODO: Use “NotificationManager” and “PendingIntent” to launch app on update
         }
+
+        context.sendBroadcast(AdblockFastApplication.blockingUpdateIntent);
     }
 }
