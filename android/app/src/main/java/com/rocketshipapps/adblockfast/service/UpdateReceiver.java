70ff801869289fd4d321368d332e805fa46d4a3c
Brian Kennish <oldestlivingboy@gmail.com>
2024-08-15 00:58:53 -0400

    Fixed updating.

--- android/app/src/main/java/com/rocketshipapps/adblockfast/service/UpdateReceiver.java
+++ android/app/src/main/java/com/rocketshipapps/adblockfast/service/UpdateReceiver.java
@@ -10,21 +10,32 @@ import com.rocketshipapps.adblockfast.AdblockFastApplication;
 public class UpdateReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_PACKAGE_REPLACED.equals(intent.getAction())) {
-            Uri data = intent.getData();
+        String action = intent.getAction();
 
-            if (data != null) {
-                if ("com.rocketshipapps.adblockfast".equals(data.getSchemeSpecificPart())) {
-                    AdblockFastApplication.dumpPrefs();
-                    AdblockFastApplication.updateLegacyPrefs(context);
-                    AdblockFastApplication.dumpPrefs();
-                    AdblockFastApplication.initPrefs();
-                    AdblockFastApplication.dumpPrefs();
-                }
+        if (action != null) {
+            // TODO: Use “NotificationManager” and “PendingIntent” to launch app on update
+            switch (action) {
+                case Intent.ACTION_MY_PACKAGE_REPLACED:
 
+                AdblockFastApplication.dumpPrefs();
+                AdblockFastApplication.updateLegacyPrefs(context);
+                AdblockFastApplication.dumpPrefs();
+                AdblockFastApplication.initPrefs();
+                AdblockFastApplication.dumpPrefs();
                 context.sendBroadcast(AdblockFastApplication.blockingUpdateIntent);
 
-                // TODO: Use “NotificationManager” and “PendingIntent” to launch app on update
+                break;
+
+                case Intent.ACTION_PACKAGE_REPLACED:
+
+                Uri data = intent.getData();
+
+                if (
+                    data != null &&
+                        "com.sec.android.app.sbrowser".equals(data.getSchemeSpecificPart())
+                ) {
+                    context.sendBroadcast(AdblockFastApplication.blockingUpdateIntent);
+                }
             }
         }
     }
