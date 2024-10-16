fa0ac2de313cb08fce8d5822590b9baf0ae76339
Brian Kennish <oldestlivingboy@gmail.com>
2024-10-15 22:24:13 -0400

    Started adding feature flags.

--- android/app/src/main/java/com/rocketshipapps/adblockfast/AdblockFastApplication.java
+++ android/app/src/main/java/com/rocketshipapps/adblockfast/AdblockFastApplication.java
@@ -17,6 +17,9 @@ import kotlin.Unit;
 
 import org.apache.maven.artifact.versioning.ComparableVersion;
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 import com.joinmassive.sdk.MassiveClient;
 import com.joinmassive.sdk.MassiveNotificationOptions;
 import com.joinmassive.sdk.MassiveOptions;
@@ -60,6 +63,7 @@ public class AdblockFastApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
         packageName = getApplicationContext().getPackageName();
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         blockingUpdateIntent =
@@ -68,6 +72,12 @@ public class AdblockFastApplication extends Application {
                 .setData(Uri.parse("package:" + packageName));
 
         handlePrefs(this);
+        remoteConfig.setConfigSettingsAsync(
+            new FirebaseRemoteConfigSettings
+                .Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build()
+        );
         MassiveClient.Companion.init(BuildConfig.MASSIVE_API_TOKEN, this, (state) -> Unit.INSTANCE);
         OneSignal.initWithContext(this, BuildConfig.ONESIGNAL_APP_ID);
     }
